17ee377c56eb8f41fd66372f2ea031327e3724c2
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-07-21 09:19:47 +0530

    added two methods  on server

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.post('/upload', (req, res) => {
+  res.send();
+});
+
+app.get('/image', (req, res) => {
+  res.send();
+});
+
 app.listen(3000, () => console.log('server is on'));
