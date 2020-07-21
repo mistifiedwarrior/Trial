ebad24583754ba1f16f31c49296a2520aa37981a
mistifiedwarrior <bagiballia@users.noreply.github.com>
2020-07-21 09:04:57 +0530

    This is the trial.

--- try.js
+++ try.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const sub = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
